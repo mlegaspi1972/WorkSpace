82f053418eaa9d666fcb5356a8a0b14e8b69c1d5
mlegaspi1972 <mlegaspi2003@gmail.com>
2022-03-24 14:32:33 -0400

    2nd update to django files
    Updates made to settings.py, models.py,..

--- Src/models.py
+++ Src/models.py
@@ -1,1 +1,8 @@
-# models for django
+# Models for Django framework
+from django.db import models
+
+class Product(models.Model):
+    title=models.CharField(max_length=50)
+    description=models.TextField(blank=True,null=True)
+    price=models.DecimalField(decimal_places=2,max_digits=1000)
+    summary=models.TextField()
